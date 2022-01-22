2bb799788a5c9375d9014ea7cfb41006b2a6acce
Emiralic <emir.alic@outlook.com>
2022-01-22 18:44:27 +0100

    first commit

---
+++ my-app/src/data.js
@@ -0,0 +1,24 @@
+const data = {
+    products: [
+      {
+        id: '1',
+        name: 'Royal Canin Veterinary 2kg',
+        price: 24,
+        image: 'https://cdn.starwebserver.se/shops/solstadens-smadjursk/files/vhn-gastrointestinal_recovery_cat_dog_loaf_can-packshot_med-_res-___basic_232195_grande.jpg',
+      },
+      {
+        id: '2',
+        name: 'Royal Canin Mini Puppy 2kg',
+        price: 18,
+        image: 'https://www.zoo.se/media/catalog/product/cache/7d22b37821d993886ccbf2e15a71358a/r/o/royal_canin_mini_puppy.jpg',
+      },
+      {
+        id: '3',
+        name: 'Trixie Trimsax',
+        price: 19,
+        image: 'https://dogmania.se/bilder/artiklar/liten/2606_S.jpg',
+      },
+    ],
+  };
+  export default data;
+  
